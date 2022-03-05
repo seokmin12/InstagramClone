8765317d77718c2674d6eb741c71ff6873428820
seokmin12 <dltjrals13@naver.com>
2022-03-05 14:56:19 +0900

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by 이석민 on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
